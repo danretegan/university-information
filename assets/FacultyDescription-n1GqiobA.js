d9afe36010648f9f217a0a4b7e246b939db1b8eb
Dan Retegan <danretegan@yahoo.com>
2024-03-12 11:48:32 +0200

    Updates

---
+++ assets/FacultyDescription-n1GqiobA.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-KnFT-8fD.js";const i=()=>t.jsx("div",{children:"Faculty Description"});export{i as default};
---
+++ assets/FacultyHistory-ZLJnaN9D.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-KnFT-8fD.js";const s=()=>t.jsx("div",{children:"Faculty History"});export{s as default};
